d49eca3cb95bf10b2120a5fdb8eded3b885fb1d4
Piotr Kaczmarski <piotr.kaczmarski.ext@assetmax.ch>
2024-07-04 13:56:37 +0200

    1.11 Custom Handlers

--- main.go
+++ main.go
@@ -9,12 +9,14 @@ func main() {
 
     serveMux := http.NewServeMux()
 
-    //serveMux.HandleFunc("/", func(resp http.ResponseWriter, req *http.Request) {
-    //    http.NotFound(resp, req)
-    //})
+    serveMux.HandleFunc("/healthz", func(resp http.ResponseWriter, req *http.Request) {
+        resp.Header()["Content-Type"] = []string{"text/plain; charset=utf-8"}
+        resp.WriteHeader(200)
+        resp.Write([]byte("OK"))
+    })
 
-    serveMux.Handle("/", http.FileServer(http.Dir(".")))
-    serveMux.Handle("/assets", http.FileServer(http.Dir("./assets")))
+    serveMux.Handle("/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+    serveMux.Handle("/assets", http.StripPrefix("/app", http.FileServer(http.Dir("./assets"))))
 
     server := http.Server{ 
         Addr: "0.0.0.0:8080",
